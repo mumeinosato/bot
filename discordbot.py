1fe981a3b7ba9d0bfbfe86bd888eb4e333a8236b
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-07-07 19:41:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -100,6 +100,39 @@ async def wiki(ctx, *,arg:str=""):
     elif arg == "TJAPlayer3":
         await embox("TJAPlayer3","TJAPlayer3 とは、\nWindows向けの太鼓の達人エミュレーターの一つ。\n現在は配布を終了している。(Waybackmachineというツールを使用すればDL可)\n.tja 形式の譜面データと音源ファイルを用意することでプレイ可能。",0x77aa27,ctx.message)
 
+        
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        # もし、送信者がbotなら無視する
+        return
+    GLOBAL_CH_NAME = "mumeinosato-global" # グローバルチャットのチャンネル名
+
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+
+        await message.delete() # 元のメッセージは削除しておく
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+        # channelsはbotの取得できるチャンネルのイテレーター
+        # global_channelsは hoge-global の名前を持つチャンネルのリスト
+
+        embed = discord.Embed(title="hoge-global",
+            description=message.content, color=0x00bfff)
+
+        embed.set_author(name=message.author.display_name, 
+            icon_url=message.author.avatar_url_as(format="png"))
+        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            icon_url=message.guild.icon_url_as(format="png"))
+        # Embedインスタンスを生成、投稿者、投稿場所などの設定
+
+        for channel in global_channels:
+            # メッセージを埋め込み形式で転送
+            await channel.send(embed=embed)
+            
 @bot.event
 async def on_message(message):
     """
