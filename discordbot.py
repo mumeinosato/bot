78a6d256671dd0fdadd7a70c9557c915d6b52adf
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-09-06 13:02:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -194,6 +194,9 @@ async def on_message(message):
         
     elif message.content.startswith("こんばん"):
         await message.channel.send("こんばんわんこそば")
+        
+    elif message.content.startswith("mu:partnerserver"):
+        await message.channel.send("yysgsgsgs")
                                  
        
     await bot.process_commands(message)#on_messageの定義内の最後にこれを入れないと定義したコマンドが動かなくなる。注意
