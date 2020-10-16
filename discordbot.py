e7645226cc60abacb95b98b757caa767b492675c
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-10-16 19:52:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -182,6 +182,20 @@ async def on_message(message):
     if message.author.bot:
         return
 
+    if message.channel.name == GLOBAL_CH_NAME:                                                          
+        print("success")
+        await message.delete() # 元のメッセージは削除しておく
+        channels = bot.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+        embed = discord.Embed(title="hoge-global",
+            description=message.content, color=0x00bfff)
+        embed.set_author(name=message.author.display_name,                                                                            
+            icon_url=message.author.avatar_url_as(format="png"))
+        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            icon_url=message.guild.icon_url_as(format="png"))# Embedインスタンスを生成、投稿者、投稿場所などの設定
+        for channel in global_channels:# メッセージを埋め込み形式で転送
+            await channel.send(embed=embed)
+    
     if bot.user in message.mentions:
         print(f"{message.author.name}にメンションされました")
         await message.channel.send(f"{message.author.mention} ヘルプが必要なのか？\nmu:help でヘルプを表示しろよ")
