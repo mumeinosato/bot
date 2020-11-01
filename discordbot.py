c2171eb1cbeb24449f2d3cc6067cbfccda101df4
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2020-11-01 13:41:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,17 +44,17 @@ async def greet():
 async def help(ctx):#コマンドを定義するときの関数は必ずContextという引数が渡される。つまり引数を最低一つだけでも書いておかないと動かないので注意
     embed = discord.Embed(title="ヘルプ", description="このヘルプコマンドにはプレフィックスを書いていないため、\n実行には全て`mu:コマンド名`とする必要があります。",color=0x4169e1)
     #↑ここのテキストは自分で修正よろしく
-    embed.add_field(name="help", value="このコマンドです。",inline=False)
-    embed.add_field(name="newinfo", value="新着情報を確認します。",inline=False)
-    embed.add_field(name="wiki", value="開発者が知っていること、関係することについてwiki形式で見ることができます。",inline=False)
-    embed.add_field(name="コマンド無し", value="「mumeinosato-global」というチャンネルを作成するとグローバルチャットに参加できます。",inline=False)
-    embed.add_field(name="game", value="このbotでできるゲーム一覧を表示します。",inline=False)
-    embed.add_field(name="tool", value="便利ツール一覧を表示します。",inline=False)
-    embed.add_field(name="about", value="botについてや、botの招待リンクを確認できます。",inline=False)
-    embed.add_field(name="support", value="この botのサポートサーバーを表示できます。",inline=False)
-    embed.add_field(name="partnerserver", value="提携サーバーを表示します。",inline=False)
-    embed.add_field(name="servermanagement", value="サーバー運営に役立つコマンド一覧を表示します。", inline=False)
-    embed.add_field(name="myinformation 〔パスワード〕", value="登録内容を表示します。（パスワードを持っている場合のみアクセスできます。)",inline=False)
+    embed.add_field(name="**help**", value="このコマンドです。",inline=False)
+    embed.add_field(name="**newinfo**", value="新着情報を確認します。",inline=False)
+    embed.add_field(name="**wiki**", value="開発者が知っていること、関係することについてwiki形式で見ることができます。",inline=False)
+    embed.add_field(name="**コマンド無し**", value="「mumeinosato-global」というチャンネルを作成するとグローバルチャットに参加できます。",inline=False)
+    embed.add_field(name="**game**", value="このbotでできるゲーム一覧を表示します。",inline=False)
+    embed.add_field(name="**tool**", value="便利ツール一覧を表示します。",inline=False)
+    embed.add_field(name="**about**", value="botについてや、botの招待リンクを確認できます。",inline=False)
+    embed.add_field(name="**support**", value="この botのサポートサーバーを表示できます。",inline=False)
+    embed.add_field(name="**partnerserver**", value="提携サーバーを表示します。",inline=False)
+    embed.add_field(name="**servermanagement**", value="サーバー運営に役立つコマンド一覧を表示します。", inline=False)
+    embed.add_field(name="**myinformation 〔パスワード〕**", value="登録内容を表示します。（パスワードを持っている場合のみアクセスできます。)",inline=False)
     await ctx.send(embed=embed)#Contextにはいろいろな情報が入っており、そこから様々な関数、情報にアクセスできる。ctx.sendがその一つ
 
 @bot.command()
